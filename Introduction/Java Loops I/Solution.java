e5ef830b0f891227ad1a9b29109af2398f2c736f
mvw21 <53233579+mvw21@users.noreply.github.com>
2021-03-02 23:40:37 +0200

    Update Solution.java

--- Introduction/Java Loops I/Solution.java
+++ Introduction/Java Loops I/Solution.java
@@ -1,6 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int multiplier = scan.nextInt();
